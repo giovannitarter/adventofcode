607cf4da90fd82c3ecc74d3c007d9a0d46240120
Giovanni Tarter <giovanni.tarter@gmail.com>
2020-12-12 07:58:23 +0100

    12 - sol1

--- 2020/12/manhattan.py
+++ 2020/12/manhattan.py
@@ -11,5 +11,55 @@ fd.close()
 lines = text.split("\n")
 lines = [l for l in lines if l != ""]
 
+
+def parse_line(text):
+    cmd = text[0]
+    val = int(text[1:])
+    return((cmd, val))
+
+
+inst = []
 for l in lines:
-    print(l)
+    inst.append(parse_line(l))
+
+
+
+x = 0
+y = 0
+ang = 90
+
+incs = {
+    "N" : (0, 1, 0),
+    "S" : (0, -1, 0),
+    "E" : (1, 0, 0),
+    "W" : (-1, 0, 0),
+    "L" : (0, 0, -1),
+    "R" : (0, 0, 1)
+}
+
+card = {
+    0 : "N",
+    90 : "E",
+    180 : "S",
+    270 : "W",
+    }
+
+
+for cmd, val in inst:
+    
+    if cmd == "F":
+        cmd = card[ang]
+
+    dx, dy, dang = incs[cmd]
+    x = x + val * dx
+    y = y + val * dy
+    ang = (ang + val * dang) % 360
+
+print(x,y)
+print("sol1:", abs(x)  + abs(y))
+
+
+
+
+
+
