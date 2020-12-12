437a0a246cd584490b760c2bf2b72753aca42635
Giovanni Tarter <giovanni.tarter@gmail.com>
2020-12-12 07:35:17 +0100

    modified:   seats.py 	new file:   ../12/input 	new file:   ../12/manhattan.py 	new file:   ../12/test

--- 2020/11/seats.py
+++ 2020/11/seats.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python
 
 import sys
-
-
-fd = open(sys.argv[1])
-text = fd.read()
-fd.close()
-
-
-lines = text.split("\n")
-lines = [l for l in lines if l != ""]
-
-mat = []
-for l in lines:
-    mat.append(list(l))
+import copy
 
 
 def print_mat(mat):
-    print("\n====================")
+    
+    print("=====================")
+    
+    nrs = [" "] + [str(n) for n in list(range(0, len(mat[0])))]
+    print(" ".join(nrs))
+        
     for idx, i in enumerate(mat):
-        print(idx, "".join(i))
-    print("====================")
+        print(idx, " ".join(i))
+    print("=====================")
 
 
 def replace_mat(mat, x, y, c):
@@ -48,107 +41,16 @@ def neighbors(mat, px, py):
             res.append((x,y))
 
     res.remove((px,py))
-    print("neigh ({},{}): {}".format(px, py, res))
-    return res
-
-
-def neighbors2(mat, px, py):
-
-    res = []
-    max_x = len(mat[0]) - 1
-    max_y = len(mat) - 1
-
-    #west <-
-    for x in range(px -1, -1, -1):
-        if mat[py][x] != ".":
-            res.append((x, py))
-            break
-
-    #east ->
-    for x in range(px + 1, len(mat[0])):
-        if mat[py][x] != ".":
-            res.append((x, py))
-            break
-    #north
-    for y in range(py-1, -1, -1):
-        if mat[y][px] != ".":
-            res.append((px, y))
-            break
-    #south
-    for y in range(py +1, len(mat)):
-        if mat[y][px] != ".":
-            res.append((px, y))
-            break
-
-    #south / east
-    max_off = min(len(mat[0])-px, len(mat)-py)
-    for off in range(1, max_off):
-        x = px+off
-        y = py+off
-        if mat[y][x] != ".":
-            res.append((x, y))
-            break
-
-    #south / west
-    max_off = min(px, len(mat)-py)+1
-    for off in range(1, max_off):
-        x = px-off
-        y = py+off
-
-        if x>=len(mat[0]) or y >=len(mat):
-            break
-
-        if mat[y][x] != ".":
-            res.append((x, y))
-            break
-
-    #north / east
-    max_off = min(py, len(mat)-px) + 1
-    #print(max_off)
-    for off in range(1, max_off):
-        x = px+off
-        y = py-off
-
-        if x>=len(mat[0]) or y >=len(mat):
-            break
-
-        if mat[y][x] != ".":
-            res.append((x, y))
-            break
-
-    #north / west
-    max_off = min(px, py)+1
-    #print(max_off)
-    for off in range(1, max_off):
-        x = px-off
-        y = py-off
-
-        if x>=len(mat[0]) or y >=len(mat):
-            break
-
-        if mat[y][x] != ".":
-            res.append((x, y))
-            break
-
-    return res
-
-
-def all_mat(mat):
-    res = []
-    for r in mat:
-        res.append(list(r))
+    #print("neigh ({},{}): {}".format(px, py, res))
     return res
 
 
 def step(mat, max_occ=4, neig_func=neighbors):
 
-    new_mat = all_mat(mat)
-    #print_mat(mat)
+    new_mat = copy.deepcopy(mat)
 
     for x in range(0, len(mat[0])):
         for y in range(0, len(mat)):
-
-            #print("aa,", x, y, mat[y][x])
 
             neigh_c = neig_func(mat, x, y)
 
@@ -159,21 +61,11 @@ def step(mat, max_occ=4, neig_func=neighbors):
                 occ = [mat[ya][xa] for xa, ya in neigh_c if mat[ya][xa] == "#"]
                 if len(occ) == 0:
                     replace_mat(new_mat, x, y, "#")
-                else:
-                    replace_mat(new_mat, x, y, "L")
 
             elif mat[y][x] == "#":
                 occ = [mat[ya][xa] for xa, ya in neigh_c if mat[ya][xa] == "#"]
                 if len(occ) >= max_occ:
                     replace_mat(new_mat, x, y, "L")
-                else:
-                    replace_mat(new_mat, x, y, "#")
-
-
-
-            #print([mat[ya][xa] for xa,ya in neigh_c])
-
-            #print_mat(new_mat)
 
     return new_mat
 
@@ -192,29 +84,80 @@ def compare_mat(mat1, mat2):
     return(tmp1 == tmp2)
 
 
-#print(test)
-#print(neighbors2(mat, *test))
+def neighbors3(mat, px, py):
+    res = []
+
+    mx = len(mat[0])
+    my = len(mat)
+
+    def ck_bound(n, mn):
+        return(n > -1 and n < mn)
+
+    incs = [
+            (0,1), (1,0), (0,-1), (-1,0),
+            (1,1), (1,-1), (-1,1), (-1,-1),
+            ]
+
+    for inc_x, inc_y in incs:
+        off = 1
+        
+        while True:
+            
+            x = px + inc_x * off
+            if not ck_bound(x, mx):
+                break;
+            
+            y = py + inc_y * off
+            if not ck_bound(y, my):
+                break;
+
+            if mat[y][x] != ".":
+                res.append((x,y))
+                break
+            
+            off = off + 1
+
+    return res
 
 
-print_mat(mat)
-next_mat = step(mat, neig_func=neighbors2, max_occ=5)
+################################
+# MAIN
+################################
+
+fd = open(sys.argv[1])
+text = fd.read()
+fd.close()
+
+
+lines = text.split("\n")
+lines = [l for l in lines if l != ""]
+
+last_mat = []
+for l in lines:
+    last_mat.append(list(l))
+mat = copy.deepcopy(last_mat)
+
+print_mat(last_mat)
+
+next_mat = step(last_mat, neig_func=neighbors3, max_occ=5)
+print_mat(next_mat)
 
 i = 1
-#while compare_mat(next_mat, mat) == False and i < 50:
-while compare_mat(next_mat, mat) == False:
+while compare_mat(next_mat, last_mat) == False:
     i = i+1
-    mat = next_mat
-    next_mat = step(mat, neig_func=neighbors2, max_occ=5)
     print(i)
+    last_mat = next_mat
+    next_mat = step(last_mat, neig_func=neighbors3, max_occ=5)
     print_mat(next_mat)
 
 count = 0
-for i in mat:
+for i in last_mat:
     count += i.count("#")
-#
+
+print("sol2:")
 print(count)
 
-sys.exit(1)
+#sys.exit(1)
 
 #sol1
 print_mat(mat)
@@ -229,21 +172,8 @@ count = 0
 for i in next_mat:
     count += i.count("#")
 
+print("sol1:")
 print(count)
 
 sys.exit(1)
-
-    #print(compare_mat(next_mat, mat))
-
-#replace_mat(mat, 0, 9, "#")
-mat = step(mat)
-#print(neighbors(mat, 9, 9))
-print_mat(mat)
-
-mat2 = step(mat)
-print_mat(mat)
-
-
-print(compare_mat(mat2, mat))
-
 
