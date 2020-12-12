040849d56d17d8fe712a6fdcdb05ef7f8e88991d
Giovanni Tarter <giovanni.tarter@gmail.com>
2020-12-12 08:56:24 +0100

    12 - 2

--- 2020/12/manhattan.py
+++ 2020/12/manhattan.py
@@ -23,11 +23,6 @@ for l in lines:
     inst.append(parse_line(l))
 
 
-
-x = 0
-y = 0
-ang = 90
-
 incs = {
     "N" : (0, 1, 0),
     "S" : (0, -1, 0),
@@ -45,6 +40,13 @@ card = {
     }
 
 
+#sol1
+x = 0
+y = 0
+ang = 90
+
+
+
 for cmd, val in inst:
     
     if cmd == "F":
@@ -58,6 +60,67 @@ for cmd, val in inst:
 print(x,y)
 print("sol1:", abs(x)  + abs(y))
 
+
+print("")
+#sol2
+bx = 0
+by = 0
+
+wx = 10
+wy = 1
+
+sin = {
+    0 : 0,
+    90 : 1,
+    180 : 0,
+    270 : -1,
+}
+
+cos = {
+    0 : 1,
+    90 : 0,
+    180 : -1,
+    270 : 0,
+}
+
+
+for cmd, val in inst:
+    
+    if cmd == "F":
+        
+        mov_x = (wx - bx) * val
+        mov_y = (wy - by) * val
+
+        bx = bx + mov_x
+        by = by + mov_y
+        wx = wx + mov_x
+        wy = wy + mov_y
+
+    elif cmd == "R" or cmd == "L":
+        rel_x = wx - bx
+        rel_y = wy - by
+
+        if cmd == "R":
+            val = ((val * -1) % 360)
+            #print(val)
+
+        mov_x = rel_x * cos[val] - rel_y * sin[val]
+        mov_y = rel_x * sin[val] + rel_y * cos[val]
+        #print(mov_x, mov_y)
+
+        wx = bx + mov_x
+        wy = by + mov_y
+    
+    else:
+        dx, dy, dang = incs[cmd]
+        wx = wx + val * dx
+        wy = wy + val * dy
+
+
+    #print(cmd, val, bx, by, wx-bx, wy-by)
+
+print(bx, by)
+print("sol2:", abs(bx)  + abs(by))
 
 
 
