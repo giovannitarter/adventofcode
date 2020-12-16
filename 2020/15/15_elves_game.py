284cfe726794b5718788b9e975ce240e4fe9fa5d
Giovanni Tarter <giovanni.tarter@gmail.com>
2020-12-16 08:13:05 +0100

    modified:   15_elves_game.py         new file:   15_elves_seq.py

--- 2020/15/15_elves_game.py
+++ 2020/15/15_elves_game.py
@@ -15,42 +15,90 @@ seq = [int(x) for x in lines[0].split(",")]
 print(seq)
 
 
-test =  [0, 3, 6]
+#test =  [0, 3, 6]
+#test =  [1, 3, 2]
+#seq = list(test)
 
-seq = list(test)
 print(seq)
 
 print("")
 
-spoken = {}
-for i, s in enumerate(test[:-1]):
-    spoken[s] = [i + 1]
+#spoken = {}
+#for i, s in enumerate(test[:-1]):
+#    spoken[s] = [i + 1]
+#spk = test[-1]
+#
+#turn = len(seq) + 1
+#
+#while turn < 15:
+#
+#    last = seq[-1]
+#    print("\nturn: {}, last: {}".format(turn, last))
+#    print(seq)
+#    print(spoken)
+#    
+#    apparition = spoken.get(last)
+#    if apparition is None:
+#        spk = 0
+#    else:
+#        spk = apparition[-1] - apparition[-2]
+#
+#    seq.append(spk)
+#
+#
+#    print("turn: {} spk: {}".format(turn, spk))
+#
+#    turn = turn + 1
 
-turn = len(seq) + 1
 
-while turn < 15:
+#SOL1
 
-    last = seq[-1]
-    print("\nturn: {}, last: {}".format(turn, last))
-    print(seq)
-    print(spoken)
 
-    if spoken.get(last) is None:
-        spk = 0
-        seq.append(spk)
-        spoken[spk].append(turn)
-    else:
-        last_seen = spoken.get(last, [])
-        print("last seen: {}".format(last_seen))
-        nxt = last_seen[-1] - last_seen[-2]
+def number_at(seq, pos): 
+    
+    #Most inefficient ever
+    #never gonna work with sol2
+    #need an hastable as above
+    turn = len(seq) + 1
+    
+    rev_seq = list(seq)
+    rev_seq.reverse()
+    
+    while turn < pos + 1:
+   
+        if turn % 1000 == 0:
+            print(turn)
 
-        a = spoken.get(nxt, [])
-        a.append(turn)
-        spoken[nxt] = a
+        #print("\nturn: {}, seq: {}".format(turn, seq))
+    
+        last = rev_seq[0]
+    
+        if rev_seq.count(last) == 1:
+            seq.append(0)
+            rev_seq.insert(0, 0)
+        
+        else:
+            #print(last)
+            last_app = rev_seq.index(last)
+            #print("last_app: {}".format(last_app))
+            last_bef = rev_seq.index(last, last_app + 1)
+            #print("last_bef: {}".format(last_bef))
+            seq.append(last_bef - last_app)
+            rev_seq.insert(0, last_bef - last_app)
+    
+        #print("turn: {}, spk: {}".format(turn, seq[-1]))
+    
+        turn = turn + 1
 
-        seq.append(nxt)
-        spk = nxt
+    return rev_seq[0]
 
-    print("turn: {} spk: {}".format(turn, spk))
+print(number_at(seq, 2020))
+print(number_at(seq, 30000000))
 
-    turn = turn + 1
+
+
+
+
+
+
+
