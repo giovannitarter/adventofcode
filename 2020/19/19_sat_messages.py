503755a0653cc5f0a96cfdd0b55b3166894411fa
Giovanni Tarter <giovanni.tarter@praim.com>
2020-12-22 08:09:48 +0100

    19-1

--- 2020/19/19_sat_messages.py
+++ 2020/19/19_sat_messages.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import sys
-
+import re
 
 fd = open(sys.argv[1])
 text = fd.read()
@@ -9,7 +9,86 @@ fd.close()
 
 
 lines = text.split("\n")
-lines = [l for l in lines if l != ""]
+#lines = [l for l in lines if l != ""]
 
-for l in lines:
-    print(l)
+
+strings_start = lines.index("")
+rules_lines = lines[:strings_start]
+strings = lines[strings_start+1:]
+
+strings = [s for s in strings if s != ""]
+
+TERM = "tr"
+OR = "or"
+LIST = "ls"
+
+rules = {}
+for r in rules_lines:
+
+    if (m := re.match("(\d*): \"(\w*)\"", r)) is not None:
+        rules[int(m.group(1))] = (TERM, m.group(2), None)
+
+    elif (idx := r.find(" |")) != -1:
+        m = re.match("(\d*): ", r)
+        bef = [int(x) for x in r[m.end():idx].split(" ")]
+        aft = [int(x) for x in r[idx+3:].split(" ")]
+        rules[int(m.group(1))] = (OR, bef, aft)
+
+    else:
+        m = re.match("(\d*): ", r)
+        rules[int(m.group(1))] = (LIST, [int(x) for x in r[m.end():].split(" ")], None)
+
+
+def build_regexp(rule_nr, rules):
+
+    #print(rules)
+
+    tp, arg1, arg2 = rules[rule_nr]
+
+    res = ""
+
+    if tp == TERM:
+        res = arg1
+
+    elif tp == LIST:
+        tmp = [build_regexp(x, rules) for x in arg1]
+        print("tmp:", tmp)
+        res = "".join(tmp)
+
+    elif tp == OR:
+        bef = "".join([build_regexp(x, rules) for x in arg1])
+        aft = "".join([build_regexp(x, rules) for x in arg2])
+        res = "(({})|({}))".format(aft, bef)
+
+    return res
+
+
+    #print(m)
+
+print("\nRules:")
+for r in rules:
+    print("{} -> {}".format(r, rules[r]))
+
+print("\nStrings:")
+print(strings)
+
+res = build_regexp(0, rules)
+print("\nRES")
+print(res)
+print("")
+
+acc = 0
+for s in strings:
+    m = re.fullmatch(res, s)
+    #print(s, m)
+    if m is not None:
+        acc = acc + 1
+        #print("matching {}".format(s))
+
+print("SOL1")
+print(acc)
+
+#print(rules[0])
+
+
+
