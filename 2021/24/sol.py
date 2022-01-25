437485b905c332c52e86b2126411070475af5237
Giovanni Tarter <giovanni.tarter@gmail.com>
2022-01-25 07:18:33 +0100

    day 24

--- 2021/24/sol.py
+++ 2021/24/sol.py
@@ -2,15 +2,147 @@
 
 
 import sys
-import copy
-
+import itertools
 
 
 def parse_input(text):
 
-    res = None
-    lines = [ x for x in text.split("\n") if x != "" ]
+    res = []
+    lines = [x for x in text.split("\n") if x != ""]
 
+    for l in lines:
+        inst = tuple(l.split())
+        res.append(inst)
+
+    return res
+
+
+class ALU:
+
+    def __init__(self, text):
+
+        self.mem = {}
+        self.inp_idx = 0
+        self.inp = text
+        return
+
+
+    def exec(self, inst, p_op1, p_op2=None):
+
+        if p_op2 is not None:
+            if p_op2 in ["w", "x", "y", "z"]:
+                op2 = self.mem.get(p_op2, 0)
+            else:
+                op2 = int(p_op2)
+
+        op1 = self.mem.get(p_op1, 0)
+
+        if inst == "inp":
+            val = int(self.inp[self.inp_idx])
+            self.inp_idx += 1
+
+        elif inst == "add":
+            val = op1 + op2
+
+        elif inst == "mul":
+            val = op1 * op2
+
+        elif inst == "div":
+            val = op1 // op2
+
+        elif inst == "mod":
+            val = op1 % op2
+
+        elif inst == "eql":
+            val = int(op1 == op2)
+
+        else:
+            print("ERROR!")
+
+        self.mem[p_op1] = val
+
+        return
+
+
+    def get_res(self, var):
+        res = self.mem.get(var, 0)
+        return res
+
+
+def run_program(inst, inp, res_var):
+
+    alu = ALU(inp)
+
+    for idx, c in enumerate(list(inst)):
+        alu.exec(*c)
+
+    return alu.get_res(res_var)
+
+
+def extract_params(inst):
+
+    res = []
+
+    for i in range(0, len(inst), 18):
+        a = int(inst[i+4][2])
+        b = int(inst[i+5][2])
+        c = int(inst[i+15][2])
+        res.append([a,b,c])
+
+    return res
+
+
+#def exec_subunit(a, b, c, inp, z):
+#    w = inp
+#    x = ((z % 26) + b) != w
+#    print(f"x: {x}")
+#    res = (z // a * (25 * x + 1)) + (w + c) * x
+#    return res
+#
+#
+#def exec_monad(params, model_nr):
+#
+#    z = 0
+#    i = 0
+#    for p in params:
+#        z = exec_subunit(*p, model_nr[i], z)
+#        i += 1
+#
+#    return z
+
+
+def find_matching_op(PARAMS):
+
+    stack = []
+    matches = []
+    for idx, p in enumerate(PARAMS):
+        if p[0] == 1:
+            stack.append(idx)
+        elif p[0] == 26:
+            matches.append((stack.pop(), idx))
+
+    return matches
+
+
+def find_nr(params, matches, digits):
+
+    res = [None] * 14
+    for push_idx, pop_idx in matches:
+
+        pair = None
+        for pu in digits:
+            for po in digits:
+                pushed = pu + params[push_idx][2]
+                if (po - params[pop_idx][1]) == pushed:
+                    pair = pu, po
+                    break
+
+            if pair is not None:
+                res[push_idx] = pu
+                res[pop_idx] = po
+                break
+
+    res = "".join([str(r) for r in res])
     return res
 
 
@@ -24,4 +156,19 @@ if __name__ == "__main__":
 
     DATA = parse_input(TEXT)
 
-    print("res : {}".format(RES))
+    PARAMS = extract_params(DATA)
+    MATCHING_OP = find_matching_op(PARAMS)
+    MAX = find_nr(PARAMS, MATCHING_OP, range(1, 10, 1))
+    MIN = find_nr(PARAMS, MATCHING_OP, range(9, 0, -1))
+
+    SOL01 = MAX
+    print(f"sol01: {MAX}")
+
+    print(run_program(DATA, SOL01, "z"))
+
+    SOL02 = MIN
+    print(f"sol02: {MIN}")
+
+    print(run_program(DATA, SOL02, "z"))
+
+    sys.exit(0)
--- 2021/24/test.py
+++ 2021/24/test.py
@@ -6,7 +6,114 @@ sol = __import__("sol")
 
 class TestAoC(unittest.TestCase):
 
-    def test_func(self):
+    def test0(self):
+
+        inp = "5"
+        inst = (
+"""
+inp x
+mul x -1
+"""
+        )
+
+        alu = sol.ALU(inp)
+        cmds = sol.parse_input(inst)
+        for c in cmds:
+            alu.exec(*c)
+
+        res = alu.get_res("x")
+        self.assertEqual(res, -5)
+        return
+
+
+    def test1(self):
+
+        inp = ["4", "12"]
+        inst = (
+"""
+inp z
+inp x
+mul z 3
+eql z x
+"""
+        )
+
+        alu = sol.ALU(inp)
+        cmds = sol.parse_input(inst)
+        for c in cmds:
+            alu.exec(*c)
+
+        res = alu.get_res("z")
+        self.assertEqual(res, 1)
+        return
+
+
+    def test2(self):
+
+        inp = ["4", "11"]
+        inst = (
+"""
+inp z
+inp x
+mul z 3
+eql z x
+"""
+        )
+
+        alu = sol.ALU(inp)
+        cmds = sol.parse_input(inst)
+        for c in cmds:
+            alu.exec(*c)
+
+        res = alu.get_res("z")
+        self.assertEqual(res, 0)
+        return
+
+
+    def test3(self):
+
+        for i in range(0, 16):
+            inp = [i]
+
+            inst = (
+"""
+inp w
+add z w
+mod z 2
+div w 2
+add y w
+mod y 2
+div w 2
+add x w
+mod x 2
+div w 2
+mod w 2
+"""
+            )
+
+            alu = sol.ALU(inp)
+            cmds = sol.parse_input(inst)
+            for c in cmds:
+                alu.exec(*c)
+
+            res = "".join([str(alu.get_res(x)) for x in ["w", "x", "y", "z"]])
+            res = int(res, 2)
+            self.assertEqual(res, i)
+        return
+
+
+    def test4(self):
+
+        model_nr = "13579246899999"
+
+        fd = open("input", "r")
+        inst = fd.read()
+        fd.close()
+
+        cmds = sol.parse_input(inst)
+        res = sol.run_program(cmds, list(model_nr), "z")
+
+        self.assertNotEqual(res, 0)
         return
 
 
